34608659f14bf3d23584246c52716faf4d3aa965
Dmitriy <dmitriy.trochshenko@gmail.com>
2020-05-18 11:54:55 +0600

    Refactor subscriptions (#24)
    * use libtrustbridge for websub related use cases
    * fix tests for subscriptions view and use cases
    * update libtrustbridge commit hash
    * chore: simplify libtrustbridge local development and upgrade it
    Co-authored-by: Arkadiy Korotaev <koriaf@gmail.com>

--- intergov/use_cases/subscription_deregister.py
+++ intergov/use_cases/subscription_deregister.py
@@ -1,4 +1,11 @@
+from libtrustbridge.websub.domain import Pattern
+from libtrustbridge.websub.repos import SubscriptionsRepo
+
 from intergov.monitoring import statsd_timer
+
+
+class SubscriptionNotFound(Exception):
+    pass
 
 
 class SubscriptionDeregisterUseCase:
@@ -8,9 +15,15 @@ class SubscriptionDeregisterUseCase:
     on user's request removes the subscription to given url for given pattern
     """
 
-    def __init__(self, subscriptions_repo):
-        self.subscriptions = subscriptions_repo
+    def __init__(self, subscriptions_repo: SubscriptionsRepo):
+        self.subscriptions_repo = subscriptions_repo
 
     @statsd_timer("usecase.SubscriptionDeregisterUseCase.execute")
-    def execute(self, url, pattern):
-        return self.subscriptions.delete(url, pattern) > 0
+    def execute(self, url, predicate):
+        pattern = Pattern(predicate=predicate)
+        subscriptions = self.subscriptions_repo.get_subscriptions_by_pattern(pattern)
+        subscriptions_by_url = [s for s in subscriptions if s.callback_url == url]
+        if not subscriptions_by_url:
+            raise SubscriptionNotFound()
+        self.subscriptions_repo.bulk_delete([pattern.to_key(url)])
+
