9280516a3bde30a22e90e572f0ecabe39039af33
Arkadiy Korotaev <koriaf@gmail.com>
2020-05-27 22:08:05 +0200

    fix(subscription): use Pattern object correctly for the subscription unregister use case

--- intergov/use_cases/subscription_deregister.py
+++ intergov/use_cases/subscription_deregister.py
@@ -20,10 +20,9 @@ class SubscriptionDeregisterUseCase:
 
     @statsd_timer("usecase.SubscriptionDeregisterUseCase.execute")
     def execute(self, url, predicate):
-        pattern = Pattern(predicate=predicate)
+        pattern = Pattern(predicate)
         subscriptions = self.subscriptions_repo.get_subscriptions_by_pattern(pattern)
         subscriptions_by_url = [s for s in subscriptions if s.callback_url == url]
         if not subscriptions_by_url:
             raise SubscriptionNotFound()
         self.subscriptions_repo.bulk_delete([pattern.to_key(url)])
-
