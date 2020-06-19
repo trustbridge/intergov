4e1712696e3826157901c7f7546f20ffcddb1f0e
Arkadiy Korotaev <koriaf@gmail.com>
2020-06-19 21:40:23 +0200

    feat(channels): Message_rx_api endpoints to receive channel messages notifications and subscription verifications

--- intergov/apis/message_rx/message.py
+++ intergov/apis/message_rx/message.py
@@ -9,7 +9,7 @@ from intergov.domain.wire_protocols.generic_discrete import Message
 from intergov.monitoring import statsd_timer
 from intergov.serializers import generic_discrete_message as ser
 from intergov.use_cases import EnqueueMessageUseCase
-
+from intergov.loggers import logging
 
 from intergov.apis.message_rx.conf import Config
 from intergov.apis.common.utils import routing
@@ -23,8 +23,10 @@ from intergov.apis.common.errors.api.message import (
 
 
 blueprint = Blueprint('message', __name__)
+logger = logging.getLogger(__name__)
 
 
+# this one is probably to be disabled very soon
 @statsd_timer("api.message_rx.endpoint.message")
 @blueprint.route('/messages', methods=['POST'])
 @routing.mimetype(['application/json'])
@@ -68,3 +70,38 @@ def message():
         )
     else:
         raise UnableWriteToInboxError()
+
+
+@statsd_timer("api.message_rx.endpoint.channel_message_confirm")
+@blueprint.route('/channel-message', methods=['GET'])
+# @routing.mimetype(['application/x-www-form-urlencoded'])
+def channel_message_confirm():
+    """
+    Handles subscription verification requests
+    https://www.w3.org/TR/websub/#hub-verifies-intent
+    hub.mode=subscribe&hub.topic=jurisdiction.AU&
+    hub.challenge=e35ad43c-7149-45cb-a8eb-3a5a76a368de&
+    hub.lease_seconds=432000
+    """
+    # TODO: validate topic and mode
+    # TODO: validate signature header
+    return Response(request.args.get('hub.challenge'))
+
+
+@statsd_timer("api.message_rx.endpoint.channel_message_receive")
+@blueprint.route('/channel-message', methods=['POST'])
+@routing.mimetype(['application/json'])
+def channel_message_receive():
+    """
+    Handles the pings
+    """
+    body = request.get_json(silent=True)
+    new_ch_message_id = body["id"]
+    # TODO: retrieve that message from the channel worker
+    # (using JWT auth we have)
+    logger.warning(
+        "Got notification about channel message %s but don't processing it yet"
+        " (don't even know the channel)",
+        new_ch_message_id
+    )
+    return Response()
